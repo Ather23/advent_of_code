4fe20e7371ccd0e294dc5eb9d2fa6e17a68d43af
ather <rizviather6@gmail.com>
2023-12-06 01:51:55 -0500

    working day2 part1

--- 2023/day2/src/main.rs
+++ 2023/day2/src/main.rs
@@ -37,14 +37,16 @@ struct Game {
 
 fn main() {
     println!("Starting..");
-    // let games: Vec<Game> = read_to_string("test1.txt")
-    //     .unwrap()
-    //     .lines()
-    //     .map(|line| { parse_file(&String::from(line)) })
-    //     .collect();
+    let games: usize = read_to_string("problem1.txt")
+        .unwrap()
+        .lines()
+        .map(|line| { parse_file(&String::from(line)) })
+        .sum();
+
+    println!("Answer: {}", games);
 }
 
-fn parse_file(game_string: &String) -> Game {
+fn parse_file(game_string: &String) -> usize {
     let mut moves_vec: Vec<Vec<Cube>> = Vec::new();
 
     // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -54,25 +56,39 @@ fn parse_file(game_string: &String) -> Game {
     );
 
     let game_idx = &game_string[space..colon].trim().to_string();
-    println!("Game {game_idx}");
+    // println!("Game {game_idx}");
     let move_string: &str = &game_string[colon + 2..game_string.len()].trim();
-    println!("Move {move_string}");
+    // println!("Move {move_string}");
 
     for moves in move_string.split(';') {
         for cubes in moves.split(';') {
             let mut cubez: Vec<Cube> = Vec::new();
             for cube in cubes.split(',') {
                 let cube_info: Vec<&str> = cube.trim().split(' ').collect();
-                println!("Cube info: {:?}", &cube_info);
+                // println!("Cube info: {:?}", &cube_info);
                 let color = cube_info.last().unwrap().trim();
-                let count = cube_info.first().unwrap().trim();
+                let count_str = cube_info.first().unwrap().trim();
+                let count = count_str.to_string().parse::<i8>().unwrap();
+
                 match color {
-                    "blue" =>
-                        cubez.push(Cube::Blue { count: count.to_string().parse::<i8>().unwrap() }),
-                    "red" =>
-                        cubez.push(Cube::Red { count: count.to_string().parse::<i8>().unwrap() }),
-                    "green" =>
-                        cubez.push(Cube::Green { count: count.to_string().parse::<i8>().unwrap() }),
+                    "blue" => {
+                        cubez.push(Cube::Blue { count: count });
+                        if count > 14 {
+                            return 0;
+                        }
+                    }
+                    "red" => {
+                        cubez.push(Cube::Red { count: count });
+                        if count > 12 {
+                            return 0;
+                        }
+                    }
+                    "green" => {
+                        cubez.push(Cube::Green { count: count });
+                        if count > 13 {
+                            return 0;
+                        }
+                    }
                     _ => {
                         panic!("{color} Not found ");
                     }
@@ -82,10 +98,11 @@ fn parse_file(game_string: &String) -> Game {
         }
     }
 
-    return Game {
-        id: game_idx.parse::<usize>().unwrap(),
-        moves: moves_vec,
-    };
+    return game_idx.parse::<usize>().unwrap();
+    // return Game {
+    //     id: game_idx.parse::<usize>().unwrap(),
+    //     moves: moves_vec,
+    // };
 }
 
 fn game_idx(game_string: String) -> String {
@@ -104,12 +121,13 @@ mod tests {
         );
 
         let game = parse_file(&test_string);
-        let moves = game.moves.first().unwrap();
-        let first_mv = moves.first().unwrap();
-        println!("{:?}", moves[0]);
+        assert_eq!(game, 12)
+        // let moves = game.moves.first().unwrap();
+        // let first_mv = moves.first().unwrap();
+        // println!("{:?}", moves[0]);
 
-        assert_eq!(game.id, 12);
-        assert!(first_mv == &(Cube::Blue { count: 3 }));
-        assert!(moves[1] == Cube::Red { count: 4 });
+        // assert_eq!(game.id, 12);
+        // assert!(first_mv == &(Cube::Blue { count: 3 }));
+        // assert!(moves[1] == Cube::Red { count: 4 });
     }
 }
